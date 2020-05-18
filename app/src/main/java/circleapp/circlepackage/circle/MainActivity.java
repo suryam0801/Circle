49b0b57a3356d9589138f2829605f10f4149e15e
suryam0801@gmail.com <58582997+suryam0801@users.noreply.github.com>
2020-05-18 13:04:18 +0530

    creation surya branch

--- app/src/main/java/circleapp/circlepackage/circle/MainActivity.java
+++ app/src/main/java/circleapp/circlepackage/circle/MainActivity.java
@@ -17,6 +17,6 @@ public class MainActivity extends AppCompatActivity {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
 
-        myRef.setValue("Hello, World!"); 
+        myRef.setValue("Hello, World!");
     }
 }
