35b18fc74a59407ed7f64cb1d955d593c44819f2
suryam0801@gmail.com <58582997+suryam0801@users.noreply.github.com>
2020-06-04 17:35:14 +0530

    Custom OTP activity

---
+++ app/src/main/java/circleapp/circlepackage/circle/Login/EntryPage.java
@@ -0,0 +1,19 @@
+package circleapp.circlepackage.circle.Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import circleapp.circlepackage.circle.R;
+
+public class EntryPage extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry_page);
+    }
+}
