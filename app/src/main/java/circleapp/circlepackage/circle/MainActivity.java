270ad009ee32d26bbda4d45df6eb6d862ef9fbef
suryam0801@gmail.com <58582997+suryam0801@users.noreply.github.com>
2020-06-14 21:50:24 +0530

    application updated

--- app/src/main/java/circleapp/circlepackage/circle/MainActivity.java
+++ app/src/main/java/circleapp/circlepackage/circle/MainActivity.java
@@ -1,47 +1,42 @@
 package circleapp.circlepackage.circle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import android.app.Activity;
-import android.content.Context;
+import android.app.Notification;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.graphics.PixelFormat;
 import android.os.Bundle;
+import android.text.Spanned;
 import android.util.Log;
 import android.view.WindowManager;
 
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.gson.Gson;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
 import circleapp.circlepackage.circle.Explore.ExploreTabbedActivity;
 import circleapp.circlepackage.circle.Helpers.AnalyticsLogEvents;
 import circleapp.circlepackage.circle.Login.EntryPage;
 import circleapp.circlepackage.circle.ObjectModels.User;
+import static circleapp.circlepackage.circle.App.CHANNEL_1_ID;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
     private FirebaseDatabase database;
-    private DatabaseReference usersDB;
+    private DatabaseReference usersDB, notificationDB;
     AnalyticsLogEvents analyticsLogEvents;
+    private NotificationManagerCompat notificationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,8 +45,11 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getWindow().setFormat(PixelFormat.RGB_565);
         analyticsLogEvents = new AnalyticsLogEvents();
+        notificationManager = NotificationManagerCompat.from(this);
 
         database = FirebaseDatabase.getInstance();
+        notificationCountGetter();
+
         SharedPreferences prefs = getApplicationContext().getSharedPreferences("PERSISTENCECHECK", Activity.MODE_PRIVATE);
         if (prefs.getBoolean(MainActivity.class.getCanonicalName(), true)) {
             prefs.edit().putBoolean(MainActivity.class.getCanonicalName(),false).apply();
@@ -87,5 +85,89 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(MainActivity.this, EntryPage.class));
             finish();
         }
+    }
+
+    public void notificationCountGetter(){
+        int counter = 0;
+        notificationDB = database.getReference().child("Notifications").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        notificationDB.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                notificationListener((int) dataSnapshot.getChildrenCount());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public void notificationListener(int count){
+        final int[] c = {0};
+        notificationDB = database.getReference().child("Notifications").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        notificationDB.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                circleapp.circlepackage.circle.ObjectModels.Notification notification = dataSnapshot.getValue(circleapp.circlepackage.circle.ObjectModels.Notification.class);
+                if(c[0] >= count){
+                    sendOnChannel1(notification);
+                } else {
+                    c[0] = c[0] +1;
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public void sendOnChannel1(circleapp.circlepackage.circle.ObjectModels.Notification notif) {
+        Log.d(TAG, "SENDING FIRST NOTIF!!!!");
+        String state = notif.getState();
+        String name = "";
+        String description = "";
+
+        switch (state) {
+            case "Accepted":
+                name = "Application Accepted";
+                description = "Accepted into " + notif.getCircleName();
+                break;
+            case "Rejected":
+                name = "Application Rejected";
+                description = "Denied from: " + notif.getCircleName();
+                break;
+            case "broadcast_added":
+                name = "New BroadCast Added";
+                description = "New broadcast in: " + notif.getCircleName();
+                break;
+        }
+
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle(name)
+                .setContentText(description)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+        notificationManager.notify(1, notification);
     }
 }
