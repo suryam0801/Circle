aa02fc841dc28f58b7e5d90434a3d18ae831bbb4
suryam0801@gmail.com <58582997+suryam0801@users.noreply.github.com>
2020-06-23 22:34:51 +0530

    explore UI updated

--- app/src/main/java/circleapp/circlepackage/circle/Explore/ExploreTabbedActivity.java
+++ app/src/main/java/circleapp/circlepackage/circle/Explore/ExploreTabbedActivity.java
@@ -2,6 +2,7 @@ package circleapp.circlepackage.circle.Explore;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.TooltipCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
@@ -24,8 +25,6 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.tabs.TabItem;
-import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
