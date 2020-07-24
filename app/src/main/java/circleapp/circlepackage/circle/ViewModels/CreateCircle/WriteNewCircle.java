688a46659dcee87c88c601dc108b29a5e808ba44
Jacob <momrider69@gmail.com>
2020-07-24 23:02:11 +0530

    Create circle UI tweaked

--- app/src/main/java/circleapp/circlepackage/circle/ViewModels/CreateCircle/WriteNewCircle.java
+++ app/src/main/java/circleapp/circlepackage/circle/ViewModels/CreateCircle/WriteNewCircle.java
@@ -10,11 +10,12 @@ import circleapp.circlepackage.circle.data.ObjectModels.Circle;
 import circleapp.circlepackage.circle.data.ObjectModels.User;
 
 public class WriteNewCircle {
-    public static void writeCircleToDb(Context context, Circle circle, User user, Subscriber subscriber){
+    public WriteNewCircle(){}
+    public void writeCircleToDb(Context context, Circle circle, User user, Subscriber subscriber){
         FirebaseWriteHelper.createUserMadeCircle(circle, subscriber);
         updateUserObject(context, user, circle);
     }
-    private static void updateUserObject(Context context, User user, Circle circle){
+    private void updateUserObject(Context context, User user, Circle circle){
         int currentCreatedNo = user.getCreatedCircles() + 1;
         user.setCreatedCircles(currentCreatedNo);
         FirebaseWriteHelper.updateUser(user, context);
