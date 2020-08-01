8ff62e5b31bdd18c52cec2c7d9041b718bde2506
Jacob <momrider69@gmail.com>
2020-08-01 18:26:08 +0530

    Intermittent excel saving poll results- Dont merge yet

--- app/src/main/java/circleapp/circlepackage/circle/ui/CircleWall/CreatorPollAnswersView.java
+++ app/src/main/java/circleapp/circlepackage/circle/ui/CircleWall/CreatorPollAnswersView.java
@@ -6,8 +6,17 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.pdf.PdfDocument;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.PieData;
@@ -16,6 +25,14 @@ import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.gson.Gson;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -32,6 +49,7 @@ import circleapp.circlepackage.circle.ViewModels.FBDatabaseReads.CirclePersonnel
 public class CreatorPollAnswersView extends AppCompatActivity {
 
     private HashMap<Subscriber, String> list = new HashMap<>();
+    private Button exportButton;
     private ImageButton bckBtn;
     private GlobalVariables globalVariables = new GlobalVariables();
     private PieChart pieChart;
@@ -52,7 +70,11 @@ public class CreatorPollAnswersView extends AppCompatActivity {
         setContentView(R.layout.activity_creator_poll_answers_view);
 
         setUIElements();
-        calculatePieChartValues();
+        try {
+            calculatePieChartValues();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         setResponsesObserver();
         //init adapter
         pollResponsesRecyclerView.setLayoutManager(new LinearLayoutManager(CreatorPollAnswersView.this));
@@ -77,6 +99,7 @@ public class CreatorPollAnswersView extends AppCompatActivity {
             userResponse = poll.getUserResponse();
         else
             userResponse = new HashMap<>();
+        exportButton = findViewById(R.id.exportResults);
     }
     private void setResponsesObserver(){
         circlePersonnelViewModel = ViewModelProviders.of(this).get(CirclePersonnelViewModel.class);
@@ -94,7 +117,7 @@ public class CreatorPollAnswersView extends AppCompatActivity {
             }
         });
     }
-    private void calculatePieChartValues(){
+    private void calculatePieChartValues() throws IOException {
         //calculating percentages
         int totalValue = 0;
         for (Map.Entry<String, Integer> entry : poll.getOptions().entrySet())
@@ -114,10 +137,86 @@ public class CreatorPollAnswersView extends AppCompatActivity {
 
         pieChart.setData(pieData);
         pieDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
-
         pieChart.animateXY(1400, 1400);
         pieChart.setDrawEntryLabels(false);
+        exportButton.setOnClickListener(v->{
+            //pieChart.saveToGallery("PollResults"+broadcast.getTitle(), 100);
+            File path = Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_MOVIES);
+            File file = new File(path, "/" + "PollResults.xlsx");
+            String filepath = String.format("/mnt/sdcard/PollResults%s.pdf",broadcast.getTitle());
+            writeDataToExcelFile(file);
+            /*File file = new File(filepath);
+            try {
+                printView2PDF(pieChart,file);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }*/
+        });
     }
+    private void printView2PDF(View content, File file) throws IOException {
+        // create a new document
+        PdfDocument document = new PdfDocument();
+
+        // crate a page description
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(
+                content.getMeasuredWidth(), content.getMeasuredHeight(),1)
+                .create();
+
+        // start a page
+        PdfDocument.Page page = document.startPage(pageInfo);
+
+        // draw something on the page
+        content.draw(page.getCanvas());
+
+        // finish the page
+        document.finishPage(page);
+        document.writeTo(new FileOutputStream(file));
+        // close the document
+        document.close();
+    }
+
+    private void writeDataToExcelFile(File fileName) {
+
+        String [][] excelData = getPollResponsesForExporting();
+
+        HSSFWorkbook myWorkBook = new HSSFWorkbook();
+        HSSFSheet mySheet = myWorkBook.createSheet();
+        HSSFRow myRow = null;
+        HSSFCell myCell = null;
+
+        for (int rowNum = 0; rowNum < excelData[0].length; rowNum++){
+            myRow = mySheet.createRow(rowNum);
+
+            for (int cellNum = 0; cellNum < list.size()+1 ; cellNum++){
+                myCell = myRow.createCell(cellNum);
+                myCell.setCellValue(excelData[rowNum][cellNum]);
+            }
+        }
+        try{
+            FileOutputStream out = new FileOutputStream(fileName);
+            myWorkBook.write(out);
+            Toast.makeText(this,"See your files for the xl file", Toast.LENGTH_SHORT).show();
+            out.close();
+        }catch(Exception e){ e.printStackTrace();}
+
+    }
+
+    private String[][] getPollResponsesForExporting() {
+        String [][] excelData = new String [list.size()+1][2];
+        excelData[0][0]="Participants";
+        excelData [0][1]="Answer";
+        int i = 0;
+        for(Map.Entry<Subscriber, String> entry : list.entrySet()) {
+            Subscriber key = entry.getKey();
+            String  value = entry.getValue();
+            excelData[i][0]=key.getName();
+            excelData[i][1]=value;
+            i++;
+        }
+        return excelData;
+    }
+
     @Override
     protected void onPause() {
         super.onPause();
